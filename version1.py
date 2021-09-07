b18094d6b758c113726e5639dc91d16ba3cef2d4
Wong <Kerrie.Wong@sanofi.com>
2021-09-07 14:13:35 -0400

    Day3, working on the subtotal/groupby

--- version1.py
+++ version1.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import numpy as np
 
-xls = pd.ExcelFile('Total Program Alirocumab (REGN727) - Day 30_Final v2.xlsx')
+xls = pd.ExcelFile('Testing file 9.7.xlsx')
 df1 = pd.read_excel(xls, 'REGN Pral Clinical Studies', skiprows = [0])
 
 # df2 = pd.read_excel(xls, 'REGN Pral FTEs')
@@ -14,13 +15,40 @@ df1 = pd.read_excel(xls, 'REGN Pral Clinical Studies', skiprows = [0])
 columns = df1.columns.tolist()
 print(columns[0:3])
 
+# issues:
+# 1.) it takes a few secs to run the file, is it normal?
 # This inplace method doesn't work but the next line does- why?
-df1[[columns[0]]].fillna(method='ffill', inplace = True)
-# df1[[columns[0], columns[1], columns[2], columns[3]]] = df1[[columns[0], columns[1], columns[2], columns[3]]].fillna(method='ffill')
-# df1[[['Unnamed: 0','Unnamed: 1','Unnamed: 2','Unnamed: 3']]] = df1[['Unnamed: 0','Unnamed: 1','Unnamed: 2','Unnamed: 3']].fillna(method='ffill')
-print(df1.head())
+# df1[[columns[0]]].fillna(method='ffill', inplace = True)
 
+# Data backfills
+# Fill NA with information (fwdfill)
+info = [columns[x] for x in range(0,4)]
+df1[info] = df1[info].fillna(method='ffill')
 
-# writer = pd.ExcelWriter('new_book.xlsx')
-# df1.to_excel(writer, 'new_sheet')
-# writer.save() 
+# Fill NA with zeros
+numeric_data = [columns[x] for x in range(5,20)]
+print(numeric_data)
+df1[numeric_data] = df1[numeric_data].fillna(0)
+
+# Can use lambda function here...
+df1[''] = ""
+df1['Budget FY recal'] = df1[columns[5]] + df1[columns[6]] + df1[columns[7]] + df1[columns[8]] - df1[columns[9]]
+df1['April Reforecast FY recal'] = df1[columns[10]] + df1[columns[11]] + df1[columns[12]] + df1[columns[13]] - df1[columns[14]]
+df1['Actuals FY recal'] = df1[columns[15]] + df1[columns[16]] + df1[columns[17]] + df1[columns[18]] - df1[columns[19]]
+df1['April Reforecast FY recal'] = df1[columns[10]] + df1[columns[11]] + df1[columns[12]] + df1[columns[13]] - df1[columns[14]]
+# print(df1.head())
+
+df1.append(df1[[columns[5]]].sum().rename('Grandtotal33')).fillna('')
+
+'''--->'''
+df1.loc['GrandTotal55'] = df1[[columns[5],columns[6]]].sum()
+df1.loc['GrandTotal55'] = df1.loc['GrandTotal55'].fillna('')
+
+# for label, _df in df1.groupby(columns[5],columns[6]):
+#     print(label)
+#     print(_df)
+#     print()
+
+writer = pd.ExcelWriter('new_book.xlsx')
+df1.to_excel(writer, 'new_sheet')
+writer.save() 
