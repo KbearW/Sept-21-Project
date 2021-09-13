db583efa6ed15af50d279dc145de8fb4dc7af86c
Wong <Kerrie.Wong@sanofi.com>
2021-09-13 11:58:02 -0400

    Day6- row sum function works, need to work on the col sum function and adding the result in excel

--- version1.py
+++ version1.py
@@ -29,79 +29,58 @@ print(f'a specific location (R,C) (0,1) should be Activity:{df1.iloc[0,1]}')
 print(f'a specific location (R,C) (0,3) should be clinical study name:{df1.iloc[0,3]}')
 
 
-print('******'*60)
-# print(df1.iloc[3,2])  #'54321 Total'
-# print('Total' in df1.iloc[3,2])  #True
+print('******'*20)
 
-reforecastQ1 = 0
-reforecastQ2 = 0
-reforecastQ3 = 0
-reforecastQ4 = 0
-reforecastFY = 0
+''' Data forwardfill '''
+# info = [columns[x] for str(x) in range(0,4)]
+# df1[info] = df1[info].fillna(method='ffill')
 
-reforecast = {}
+def assign_quarterly_sum(array_name, quarter_name, row_number, column_number, sheet):
+    '''recalculate total based on clinical study number'''
+    array_name[quarter_name] = 0
+    if 'Total' in str(df1.iloc[row,0]) or 'Total' in str(df1.iloc[row,1]) or 'Total' in str(df1.iloc[row,2]):
+        pass
+    array_name[quarter_name] += df1.iloc[row, column_number]
 
 header = True
 for row in range(rows):
     if header:
         if df1.iloc[row,0] =='Budget Type': 
             header = False
-            print('label row- skip')
+            # print(row, 'label row- skip')
     else:
-        # print(df1.iloc[row,10])
-        print(row)
-        if 'Total' not in str(df1.iloc[row,0]) and 'Total' not in str(df1.iloc[row,1]) and 'Total' not in str(df1.iloc[row,2]):
-            reforecastQ1 += df1.iloc[row,10]
-            reforecastQ2 += df1.iloc[row,11]
-            reforecastQ3 += df1.iloc[row,12]
-            reforecastQ4 += df1.iloc[row,13]
-            reforecastFY += df1.iloc[row,14]
-reforecast['reforecastQ1'] = reforecastQ1
-reforecast['reforecastQ2'] = reforecastQ2
-reforecast['reforecastQ3'] = reforecastQ3
-reforecast['reforecastQ4'] = reforecastQ4
-reforecast['reforecastFY'] = reforecastFY
+        budget = {}
+        reforecast = {}
+        actual = {}
 
+        assign_quarterly_sum( budget, 'Q1', row, 5, df1)
+        assign_quarterly_sum( budget, 'Q2', row, 6, df1)
+        assign_quarterly_sum( budget, 'Q3', row, 7, df1)
+        assign_quarterly_sum( budget, 'Q4', row, 8, df1)
+        assign_quarterly_sum( budget, 'FY', row, 9, df1)
+
+        assign_quarterly_sum( reforecast, 'Q1', row, 10, df1)
+        assign_quarterly_sum( reforecast, 'Q2', row, 11, df1)
+        assign_quarterly_sum( reforecast, 'Q3', row, 12, df1)
+        assign_quarterly_sum( reforecast, 'Q4', row, 13, df1)
+        assign_quarterly_sum( reforecast, 'FY', row, 14, df1)
+        
+        assign_quarterly_sum( actual, 'Q1', row, 15, df1)
+        assign_quarterly_sum( actual, 'Q2', row, 16, df1)
+        assign_quarterly_sum( actual, 'Q3', row, 17, df1)
+        assign_quarterly_sum( actual, 'Q4', row, 18, df1)
+        assign_quarterly_sum( actual, 'FY', row, 19, df1)
+        
+# res = {}
+# res['reforecast'] = reforecast
+print(budget)
 print(reforecast)
+print(actual)
+# print(res)
 
+# df1['testing- blank'] = "123545"
 
-# reforecast = {}
-
-# header = True
-# for row in range(rows):
-#     if header:
-#         if df1.iloc[row,0] =='Budget Type': 
-#             header = False
-#             print('label row- skip')
-#     else:
-#         # print(df1.iloc[row,10])
-#         print(row)
-#         if 'Total' not in str(df1.iloc[row,0]) and 'Total' not in str(df1.iloc[row,1]) and 'Total' not in str(df1.iloc[row,2]):
-#             reforecast.get('reforecastQ1',0) += df1.iloc[row,10]
-#             reforecast.get('reforecastQ2',0) += df1.iloc[row,11]
-#             reforecast.get('reforecastQ3',0) += df1.iloc[row,12]
-#             reforecast.get('reforecastQ4',0) += df1.iloc[row,13]
-#             reforecast.get('reforecastFY',0) += df1.iloc[row,14]
-
-
-# print(reforecast)
-
-# print(reforecast)
-
-# print('+++++'*66)
-# print(df1.columns)
-# print(df1[['Activity ','Cost Type',  'April Reforecast Q1 2021']])
-
-# print(rows)
-# print('only return linew with total in activity, answer should be 1417.598705' )
-
-# print(df1[['Activity ','Cost Type',  'April Reforecast Q1 2021']][df1['Activity ']=='Clinical Total'])
-# df1.set_index('Clinical Study Number',inplace=True)
-# print(df1.head())
-
-# # print(type(columns))
-# # print(type(rows))
-# # print(df1[1:3])
+# df1['recal'] = budget
 
 # # # Fill NA with zeros
 # # numeric_data = [columns[x] for x in range(5,31)]
@@ -114,16 +93,6 @@ print(reforecast)
 # print(f"rows: {rows}")
 # print(f"columns: {columns}")
 
-# # issues:
-# # 1.) it takes a few secs to run the file, is it normal?
-# # This inplace method doesn't work but the next line does- why?
-# # df1[[columns[0]]].fillna(method='ffill', inplace = True)
-
-# # Data backfills
-# # Fill NA with information (fwdfill)
-# # info = [columns[x] for x in range(0,4)]
-# # df1[info] = df1[info].fillna(method='ffill')
-
 # # # Fill NA with zeros
 # # numeric_data = [columns[x] for x in range(5,20)]
 # # print(numeric_data)
@@ -132,29 +101,12 @@ print(reforecast)
 # # print(type(df1[columns[2]]))
 # # print(df1[columns[2].astype(basestring)])
 
-# # Can use lambda function here...
-# # df1[''] = ""
-
-# # res = {}
-# # res['Budget FY recal'] = df1[columns[5]] + df1[columns[6]] + df1[columns[7]] + df1[columns[8]] - df1[columns[9]]
-# # res['April Reforecast FY recal'] = df1[columns[10]] + df1[columns[11]] + df1[columns[12]] + df1[columns[13]] - df1[columns[14]]
-# # res['Actuals FY recal'] = df1[columns[15]] + df1[columns[16]] + df1[columns[17]] + df1[columns[18]] - df1[columns[19]]
-# # res['April Reforecast FY recal'] = df1[columns[10]] + df1[columns[11]] + df1[columns[12]] + df1[columns[13]] - df1[columns[14]]
-
-# # print(res)
-# # print(df1.head())
-
 # # df1.append(df1[[columns[5]]].sum().rename('Grandtotal33')).fillna('')
 
 # # '''--->'''
 # # df1.loc['GrandTotal'] = df1[[columns[x] for x in range(5,20)]].sum()
 # # # df1.loc['GrandTotal55'] = df1.loc['GrandTotal55'].fillna('')
 
-# # # for label, _df in df1.groupby(columns[5],columns[6]):
-# # #     print(label)
-# # #     print(_df)
-# # #     print()
-
-# # writer = pd.ExcelWriter('new_book.xlsx')
-# # df1.to_excel(writer, 'new_sheet')
-# # writer.save() 
+writer = pd.ExcelWriter('new_book.xlsx')
+df1.to_excel(writer, 'new_sheet')
+writer.save() 
